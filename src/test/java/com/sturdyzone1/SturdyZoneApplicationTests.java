bc01351ec4cc6f5d813a85e65331f7037b4b31b2
Mohd K. Alam <aryan22k09@gmail.com>
2025-08-14 20:33:08 +0530

    Innitial commit - Spring Boot Project Setup

---
+++ src/test/java/com/sturdyzone1/SturdyZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sturdyzone1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SturdyZoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
